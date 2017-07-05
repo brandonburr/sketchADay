797eb4ea38e1303b8a014645f4065bf4adb6f782
Brandon Burr <brandonburr@gmail.com>
2017-07-05 11:14:27 -0700

    sigh...tried something with 3d rendering. but i am limited to regular shapes, like balls

--- brando_20170705/sketch.js
+++ brando_20170705/sketch.js
@@ -11,6 +11,57 @@ class Utils {
 	}
 }
 
+class Sphere {
+  constructor(x, y, z) {
+    let SMALL_INCREMENT = windowWidth > 400 ? 10 : 3;
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.centerVX = 0;
+    this.centerVY = 0;
+    this.centerVZ = 0;
+    this.r = Utils.randomBetween(0, 255);
+    this.g = Utils.randomBetween(0, 255);
+    this.b = Utils.randomBetween(0, 255);
+    this.a = Utils.randomBetween(0, 180);
+    this.tick = 0;
+    this.maybeChangeVelocity = this.maybeChangeVelocity.bind(this);
+  }
+
+  maybeChangeVelocity(val, index) {
+    if(Math.random() < .1) {
+      val = Utils.randomBetween(-2, 3);
+    }
+    return val;
+  }
+
+  moveShape() {
+    // move centroid by velocity and bounce
+    this.x += this.centerVX;
+    this.y += this.centerVY;
+    this.z += this.centerVZ;
+    if(this.x > windowWidth || this.x < 0) {
+      this.centerVX *= -1;
+    }
+    if(this.y > windowHeight || this.y < 0) {
+      this.centerVY *= -1;
+    }
+    if(this.z > windowHeight || this.z < 0) {
+      this.centerVZ *= -1;
+    }
+    this.tick++;
+  }
+
+  draw() {
+    // draw rect
+    let alpha = this.a;
+    if(this.tick < 500) {
+      alpha = this.tick / 500.0 * alpha;
+    }
+    // fill(this.r, this.g, this.b, alpha);
+    sphere(50);
+  }
+}
 
 class Blob {
   constructor(x, y, z, numVertices) {
@@ -47,51 +98,32 @@ class Blob {
     return val;
   }
 
-  draw() {
-    // draw rect
-    let alpha = this.a;
-    if(this.tick < 500) {
-      alpha = this.tick / 500.0 * alpha;
-    }
-    fill(this.r, this.g, this.b, alpha);
-    beginShape();
-    vertex(this.x, this.y, this.z);
-    for(let i = 0; i < this.dx.length - 3; i += 3) {
-      bezierVertex(this.x + this.dx[i], this.y + this.dy[i], this.z + this.dz[i],
-									this.x + this.dx[i+1], this.y + this.dy[i+1], this.z + this.dz[i+1],
-										this.x + this.dx[i+2], this.y + this.dy[i+2], this.z + this.dz[i+2],);
-		}
-		// one last bezier back towards the beginning
-    bezierVertex(this.x - this.dx[1], this.y - this.dy[1], this.z + this.dz[1],
-								this.x - this.dx[0], this.y - this.dy[0], this.z + this.dz[0],
-      					this.x, this.y, this.z);
-    endShape();
-
+  moveShape() {
     // move each corner by its velocity
     Utils.eachPoint(this.dx, (val, index) => {
-			if(val > 300) {
-				this.vx[index] = -2;
-			} else if(val < -300) {
-				this.vx[index] = 2;
-			}
-			return val += this.vx[index];
-		});
+      if(val > 300) {
+      	this.vx[index] = -2;
+    	} else if(val < -300) {
+      	this.vx[index] = 2;
+    	}
+  	  return val += this.vx[index];
+	  });
     Utils.eachPoint(this.dy, (val, index) => {
       if(val > 300) {
-  	    this.vy[index] = -2;
-    	} else if(val < -300) {
-      	this.vy[index] = 2;
+	      this.vy[index] = -2;
+  	  } else if(val < -300) {
+    	  this.vy[index] = 2;
     	}
     	return val += this.vy[index];
 	  });
     Utils.eachPoint(this.dz, (val, index) => {
       if(val > 300) {
-      this.vz[index] = -2;
-    } else if(val < -300) {
-      this.vz[index] = 2;
-    }
-    return val += this.vy[index];
-  });
+  	    this.vz[index] = -2;
+    	} else if(val < -300) {
+      	this.vz[index] = 2;
+	    }
+  	  return val += this.vy[index];
+  	});
 
     // maybe change corner velocity
     Utils.eachPoint(this.vx, this.maybeChangeVelocity);
@@ -112,6 +144,24 @@ class Blob {
       this.centerVZ *= -1;
     }
     this.tick++;
+  }
+
+  draw() {
+    // draw rect
+    let alpha = this.a;
+    if(this.tick < 500) {
+      alpha = this.tick / 500.0 * alpha;
+    }
+    fill(this.r, this.g, this.b, alpha);
+    beginShape();
+    vertex(this.x, this.y, this.z);
+    for(let i = 0; i < this.dx.length; i++) {
+      vertex(this.x + this.dx[i], this.y + this.dy[i], this.z + this.dz[i]);
+		}
+		// one last vertex back towards the beginning
+    vertex(this.x, this.y, this.z);
+    endShape();
+    this.moveShape();
   }
 }
 
@@ -196,29 +246,43 @@ class MySketch {
 		let NUM_SHAPES = 10;
 		this.shapes = [];
 		this.tick = 0;
+		this.yAngle = 0;
+		this.zAngle = 0;
+		this.xAngle = 0;
+		this.run = true;
+    this.r = Utils.randomBetween(0, 255);
+    this.g = Utils.randomBetween(0, 255);
+    this.b = Utils.randomBetween(0, 255);
+    this.a = Utils.randomBetween(0, 180);
 	}
 
 	draw() {
-		noStroke();
-		let gray = (Math.sin(this.tick / 500) + 1) * 256/2;
-		background(gray);
-		// background(0, 0, 5, 20);
-		for(let i = 0; i < this.shapes.length; i++) {
-			this.shapes[i].draw();
-		}
-		this.tick++;
-		let NEW_SHAPE_TIME = 100;
-		let DELETE_SHAPE_TIME = 5000;
-		let NUM_SHAPES = 5;
-		if(this.tick % NEW_SHAPE_TIME == 1 && this.shapes.length < NUM_SHAPES) {
-			let numVertices = Utils.randomBetween(1, 8) * 3; 	// each bezier takes 3 points
-			let x = Utils.randomBetween(0, windowWidth);
-			let y = Utils.randomBetween(0, windowHeight);
-      let z = Utils.randomBetween(-windowHeight, windowHeight);
-      this.shapes.push(new Blob(x, y, z, numVertices));
-		} else if(this.tick % DELETE_SHAPE_TIME == 0) {
-			this.shapes = this.shapes.slice(1, this.shapes.length);
-		}
+	  background(this.r, this.g, this.b, this.a);
+
+    this.yAngle += 0.001;
+    this.zAngle += 0.002;
+    this.xAngle += .0001;
+    let sinVal = sin(frameCount * 0.0001 + j) * 100
+    let cosVal = cos(frameCount * 0.0001 + j) * 100
+    rotateY(this.yAngle);
+    rotateX(this.xAngle);
+    rotateZ(this.zAngle);
+
+    let rotateVal = frameCount * 0.0002;
+    for(var j = 0; j < 6; j++){
+      push();
+      for(var i = 0; i < 80; i++){
+        sinVal = sin(frameCount * 0.0001 + j) * 100
+        cosVal = cos(frameCount * 0.0001 + j) * 100
+        translate(sinVal, cosVal, i * 0.1);
+        rotateZ(rotateVal);
+        push();
+        // ambientLight(rotateVal * 255 % 255, abs(cosVal) * 5 % 255, abs(sinVal) * 5 % 255);
+        sphere(8, 6, 4);
+        pop();
+      }
+      pop();
+    }
 	}
 
 }
